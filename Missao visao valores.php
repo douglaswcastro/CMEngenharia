37904d6a1f0946543577806a345ea83ed0124dbe
douglaswcastro <douglaswcastro@hotmail.com>
2017-08-08 17:28:25 -0300

    ordenação de quem somos

--- Missao visao valores.php
+++ Missao visao valores.php
@@ -18,8 +18,16 @@
 include("cabecalho.php");
 ?>
 
-<h1 align="center">Missão</h1>
-<h3 align="center">Entrega do empreendimento dentro do prazo atendendo<br> o cronograma da obra com máxima qualidade,<br> capazes de superar a expectativa dos nossos clientes.</h3>
+<div class="row">
+    
+    <div class="col-md-4">
+    Missão
+    Entrega do empreendimento dentro do prazo atendendo<br> o cronograma da obra com máxima qualidade,<br> capazes de superar a expectativa dos nossos clientes
+
+    </div>
+</div>
+
+
 <h1 align="center">Visão</h1>
 <h3 align="center">Até 2017 nós seremos, no<br> segmento médio da construção civil,<br> uma referência em compromisso, qualidade e satisfação dos clientes.</h3>
 <h1 align="center">Valores</h1>
