f1398f3747d994292f1258c8cb799597c49b1e15
douglaswcastro <douglaswcastro@hotmail.com>
2017-09-04 21:49:26 -0300

    alterações feitas para para melhora do visual

--- footer.php
+++ footer.php
@@ -1,8 +1,7 @@
-<div id="rodape" align="right">
+<div id="rodape" align="center">
 <footer >
-	<h3> Rua Juciliano Narciso Campos, 957<br />Bosque de Ibirité, Ibirité - MG, 32400-000
-    <br />Email: contato@cmengenhariabh.com.br
-    <br />Telefone: (31) 3598-3145</h3>
+	<h3> Rua Juciliano Narciso Campos, 957 Bosque de Ibirité, Ibirité - MG, 32400-000
+    <br>Email: contato@cmengenhariabh.com.br Telefone: (31) 3598-3145</h3>
     	
 </footer> 
 <footer>
--- quemsomos.php
+++ quemsomos.php
@@ -2,7 +2,7 @@
 include("cabecalho.php");
 ?>
 <div class="row-fluid" style="margin: 20px;"> 
-	<div class="span4">
+	<div class="span3">
 	<label><h1>Missão</h1></label>
 	<label><h2>Entrega do empreendimento dentro do prazo atendendo<br>
 	o cronograma da obra com máxima qualidade,<br>
@@ -10,7 +10,7 @@ include("cabecalho.php");
 	</h2></label>
 	</div>
 
-	<div class="span4">
+	<div class="span3">
 	<label><h1>Visão</h1></label>
 	<label><h2>Até 2017 nós seremos, no<br>
 	segmento médio da construção civil,<br>
@@ -18,35 +18,23 @@ include("cabecalho.php");
 	</h2></label>
 	</div>
 
-	<div class="span4">
+	<div class="span3">
 	<label><h1>Valores</h1></label>
 	<label><h2>Segurança, conformidade,<br>
 	e confiabilidade:<br>
 	Satisfação do cliente é nosso primeiro valor.
 	</h2></label>
 	</div>
-</div>
 
-<div class="row-fluid" style="margin: 20px;">
-<div class="span12">
-	<label align="center"><h1>Ética</h1></label>
-	<label align="center"><h2>Tratamos uns aos outros e nossos clientes<br> 
-	com transparência, dignidade e respeito a vida.<br>
-	Gestão participativa: Trabalhamos num ambiente harmônico e amigável,<br>
-	com envolvimento e participação dos colaboradores para alcance<br>
-	dos objetivos e metas da empresa.</h2></label>
-</div>
-</div>
-
-
-<div class="row-fluid" style="margin: 20px;">
-<div class="span3">
+	<div class="span3">
 	<label><h1>Responsabilidade empresarial</h1></label>
 	<label><h2>Somos responsáveis pelas nossas ações<br>
 	e cumprimos nossos compromissos.</h2></label>
+	</div>
 </div>
 
-<div class="span3">
+<div class="row-fluid" style="margin: 20px;">
+<div class="span4">
 	<label><h1>Qualidade</h1></label>
 	<label><h2>Buscamos constantemente o aperfeiçoamento dos colaboradores<br>
 	através de  métodos construtivos em busca<br>
@@ -54,20 +42,27 @@ include("cabecalho.php");
 	</h2></label>
 </div>
 
-<div class="span3">
+<div class="span4">
 	<label><h1>Inovação</h1></label>
  	<h2>Facilitamos a vida do cliente através de
 	inovações tecnológicas agregadas aos produtos oferecidos.
 	</h2></label>
 </div>
 
-<div class="span3" style="pading-rigth:20px;">
+<div class="span4" style="pading-rigth:20px;">
 	<label><h1>Responsabilidade ambiental:</h1></label>
  	<h2>Respeitamos o meio ambiente seguindo </br> 
 	as normas e contribuímos
 	com a diminuição no impacto ambiental.
 	</h2></label>
 </div>
+</div>
+
+
+<div class="row-fluid" style="margin: 20px;">
+
+
+
 </div>
 <?php
 include("footer.php");
